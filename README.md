c63ca3b71bdadacbad52f5cc1e14941bb2f4b82e
MatiVivas <mativivas77@gmail.com>
2020-03-22 21:25:10 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## WEBSERVER
+
+Curso de Fernando Herrera, en Udemy, empezado el 11 de Diciembre de 2019
+
+Permite...
+
+### Temas de la sección:
+
+1- Instalación y pruebas con MongoDB
+
+2- Codigos de error de HTTP
+
+3- Códigos de error de Express
+
+4- Peticiones HTTP
+
+    a- GET
+    b- POST
+    c- PUT
+    d- DELETE
+
+5- Archivos de configuración global
+
+6- Tips importantes en Postman
+
+
+Utiliza:
+
+https://nodejs.org/api/http.html
+
+https://www.npmjs.com/package/express
+
+https://www.npmjs.com/package/hbs
---
+++ server/server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+
+require('./config/config.js');
+//method GET
+//Se utiliza para consultas
+app.get('/usuario', function (req, res) {
+  res.json('get Usuario');
+});
+
+//method POST
+//Generalmente se utiliza para crear nuevos registros de datos
+app.post('/usuario', function (req, res) {
+    let body = req.body;
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            description: "El nombre es necesario"
+        });
+
+    } else {
+        salida = {
+            method: 'POST',
+            persona: body
+        }
+        res.json(salida);
+    }
+    
+});
+
+//method PUT
+//Generalmente se utiliza para actualizar datos localhost:3000/parametro
+app.put('/usuario/:id', function (req, res) {
+    let id = req.params.id;
+    let salida = {
+        method: 'PUT',
+        id
+    }
+    res.json(salida);
+});
+
+//method PUT
+//Generalmente se utiliza para actualizar datos
+app.delete('/usuario', function (req, res) {
+    res.json('delete Usuario')
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando peticiones en el puerto ", process.env.PORT);
+})
